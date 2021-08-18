5492216754313cac927b0671eba47639514b2f91
yoyofx <zl.hxd@hotmail.com>
2021-08-18 15:53:46 +0800

    data script udpate

--- scripts/sql/platform.sql
+++ scripts/sql/platform.sql
@@ -2,10 +2,11 @@
 /*   DatabaseName = sgr_platform   */
 /*   TableName = sgr_context   */
 /******************************************/
-CREATE TABLE `sgr_context` (
+CREATE TABLE `sgr_sys_menu` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `context_code` varchar(100) NOT NULL COMMENT '编码',
-  `context_name` varchar(50) NOT NULL COMMENT '目录名称',
+  `tenant_id` bigint(11) NOT NULL COMMENT '租户',
+  `menu_code` varchar(100) NOT NULL COMMENT '编码',
+  `menu_name` varchar(50) NOT NULL COMMENT '目录名称',
   `is_root` tinyint(3) NOT NULL DEFAULT '0' COMMENT '是否是根目录',
   `parent_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '父层级id',
   `sort` int(11) NOT NULL DEFAULT '0' COMMENT '权重，正序排序',
@@ -37,7 +38,7 @@ CREATE TABLE `sgr_tenant` (
 /*   DatabaseName = sgr_platform   */
 /*   TableName = tenant_role   */
 /******************************************/
-CREATE TABLE `tenant_role` (
+CREATE TABLE `sgr_tenant_role` (
   `id` bigint(20) unsigned NOT NULL,
   `role_code` varchar(30) NOT NULL COMMENT '角色编码',
   `role_name` varchar(50) NOT NULL COMMENT '角色名称',
@@ -54,8 +55,9 @@ CREATE TABLE `tenant_role` (
 /*   DatabaseName = sgr_platform   */
 /*   TableName = tenant_user   */
 /******************************************/
-CREATE TABLE `tenant_user` (
+CREATE TABLE `sgr_tenant_user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tenant_id` bigint(11) NOT NULL COMMENT '租户',
   `user_name` varchar(50) DEFAULT NULL COMMENT '用户名',
   `account` varchar(50) NOT NULL COMMENT '账号',
   `password` varchar(255) NOT NULL COMMENT '密码',
