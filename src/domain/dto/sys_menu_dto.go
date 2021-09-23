30718dd03cb873692a2421b7a8794ed051699293
yoyofx <zl.hxd@hotmail.com>
2021-09-23 14:32:21 +0800

    角色与菜单

--- src/domain/dto/sys_menu_dto.go
+++ src/domain/dto/sys_menu_dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type SysMenuTreeDTO struct {
-	ID           uint64
+	ID           uint64 `json:"id"`
 	TenantID     int64  // 租户
 	MenuCode     string // 编码
 	MenuName     string // 目录名称
