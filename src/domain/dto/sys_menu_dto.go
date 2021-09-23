9d0c9c8ceb6b76023bde421d6c8afc4b85a33e7f
cui <291527070@qq.com>
2021-09-23 15:48:20 +0800

    添加用户菜单树获取

--- src/domain/dto/sys_menu_dto.go
+++ src/domain/dto/sys_menu_dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type SysMenuTreeDTO struct {
-	ID           uint64
+	ID           uint64 `json:"id"`
 	TenantID     int64  // 租户
 	MenuCode     string // 编码
 	MenuName     string // 目录名称
@@ -9,5 +9,19 @@ type SysMenuTreeDTO struct {
 	ParentID     uint64 // 父层级id
 	Sort         int    // 权重，正序排序
 	Status       int8   // 状态
+	Icon         string // 图标
+	Path         string // 路由路径
+	Component    string // react组件路径
 	ChildrenMenu *[]SysMenuTreeDTO
+}
+
+type SysMenuRoutes struct {
+	ID        uint64 `json:"id"`
+	Name      string
+	Path      string
+	Component string
+	Icon      string
+	Routes    *[]SysMenuRoutes
+	Layout    bool
+	Sort      int
 }
