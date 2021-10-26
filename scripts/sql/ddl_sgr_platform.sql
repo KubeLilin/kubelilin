dcfa2e5659930bb3e34392e0a670baf4392c6dfc
yoyofx <zl.hxd@hotmail.com>
2021-10-26 15:31:34 +0800

    ddl db sql

--- scripts/sql/ddl_sgr_platform.sql
+++ scripts/sql/ddl_sgr_platform.sql
@@ -1,7 +1,5 @@
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_role_menu_map   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_role_menu_map` ;
+
 CREATE TABLE `sgr_role_menu_map` (
                                      `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                                      `role_id` bigint(20) unsigned NOT NULL COMMENT '角色ID',
@@ -9,13 +7,10 @@ CREATE TABLE `sgr_role_menu_map` (
                                      `creation_time` datetime DEFAULT CURRENT_TIMESTAMP,
                                      `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                      PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COMMENT='角色菜单权限影射'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COMMENT='角色菜单权限影射';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_sys_menu   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_sys_menu` ;
+
 CREATE TABLE `sgr_sys_menu` (
                                 `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                                 `tenant_id` bigint(11) NOT NULL COMMENT '租户',
@@ -31,13 +26,10 @@ CREATE TABLE `sgr_sys_menu` (
                                 `creation_time` datetime DEFAULT CURRENT_TIMESTAMP,
                                 `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COMMENT='菜单'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COMMENT='菜单';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_tenant   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_tenant` ;
+
 CREATE TABLE `sgr_tenant` (
                               `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                               `t_name` varchar(50) NOT NULL COMMENT '租户名称',
@@ -47,13 +39,10 @@ CREATE TABLE `sgr_tenant` (
                               `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
                               PRIMARY KEY (`id`),
                               UNIQUE KEY `un_code` (`t_code`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COMMENT='租户'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COMMENT='租户';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_tenant_cluster   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_tenant_cluster` ;
+
 CREATE TABLE `sgr_tenant_cluster` (
                                       `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
                                       `tenant_id` bigint(20) unsigned DEFAULT NULL COMMENT '租户ID',
@@ -67,13 +56,10 @@ CREATE TABLE `sgr_tenant_cluster` (
                                       `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
                                       `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
                                       PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='集群信息'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='集群信息';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_tenant_namespace   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_tenant_namespace` ;
+
 CREATE TABLE `sgr_tenant_namespace` (
                                         `id` bigint(20) unsigned NOT NULL,
                                         `tenant_id` bigint(20) unsigned DEFAULT NULL COMMENT '租户ID',
@@ -83,13 +69,10 @@ CREATE TABLE `sgr_tenant_namespace` (
                                         `update_time` datetime NOT NULL COMMENT '更新时间',
                                         `status` tinyint(4) NOT NULL COMMENT '状态',
                                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='集群_命名空间'
-;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='集群_命名空间';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_tenant_role   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_tenant_role` ;
+
 CREATE TABLE `sgr_tenant_role` (
                                    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                                    `role_code` varchar(30) NOT NULL COMMENT '角色编码',
@@ -101,13 +84,10 @@ CREATE TABLE `sgr_tenant_role` (
                                    `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                                    PRIMARY KEY (`id`),
                                    UNIQUE KEY `un_role_code_name` (`role_code`,`role_name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='租户角色'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='租户角色';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_tenant_user   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_tenant_user` ;
+
 CREATE TABLE `sgr_tenant_user` (
                                    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                                    `tenant_id` bigint(11) NOT NULL COMMENT '租户',
@@ -120,13 +100,10 @@ CREATE TABLE `sgr_tenant_user` (
                                    `creation_time` datetime DEFAULT CURRENT_TIMESTAMP,
                                    `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                                    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息';
 
-/******************************************/
-/*   DatabaseName = sgr_paas   */
-/*   TableName = sgr_tenant_user_role   */
-/******************************************/
+DROP TABLE IF EXISTS `sgr_tenant_user_role` ;
+
 CREATE TABLE `sgr_tenant_user_role` (
                                         `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                                         `user_id` bigint(20) NOT NULL COMMENT '用户id',
@@ -134,5 +111,5 @@ CREATE TABLE `sgr_tenant_user_role` (
                                         `creation_time` datetime DEFAULT CURRENT_TIMESTAMP,
                                         `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='用户角色'
-;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='用户角色';
+
