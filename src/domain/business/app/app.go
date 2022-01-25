671abdba17c403dde51a28bf320e030d1bdb271e
yoyofx <zl.hxd@hotmail.com>
2022-01-25 16:53:06 +0800

    更新app，label信息

--- src/domain/business/app/app.go
+++ src/domain/business/app/app.go
@@ -56,6 +56,8 @@ func (s *ApplicationService) UpdateApp(req *req.AppReq) (error, int64) {
 	appModel.Remarks = req.Remarks
 	appModel.Language = req.Language
 	appModel.Status = req.Status
+	//appModel.Git = req.Git
+	appModel.Labels = req.Labels
 	dbRes := s.db.Model(&models.SgrTenantApplication{}).Where("id=?", req.ID).Updates(appModel)
 	if dbRes.Error != nil {
 		return nil, 0
