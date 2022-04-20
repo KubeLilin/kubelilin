33eef9b656422233e7c844fc58c34ad900161a88
yoyofx <zl.hxd@hotmail.com>
2022-04-20 20:28:48 +0800

    服务连接

--- src/domain/business/app/service_connection.go
+++ src/domain/business/app/service_connection.go
@@ -31,42 +31,23 @@ func NewServiceConnectionService(db *gorm.DB) *ServiceConnectionService {
 }
 
 func (scs *ServiceConnectionService) CreateServiceConnection(req *req.ServiceConnectionReq) (*req.ServiceConnectionReq, error) {
-	fmt.Println(req.ServiceType)
-	if req.ServiceType != 1 && req.ServiceType != 2 {
-		return nil, errors.New("请选择正确的连接类型")
-	}
 	var mainDatum = models.ServiceConnection{}
-	var credentialDatum = models.ServiceConnectionCredentials{}
 	var connectionDatum = models.ServiceConnectionDetails{}
 	mainDatum.ServiceType = req.ServiceType
 	mainDatum.Name = req.Name
 	mainDatum.TenantID = req.TenantID
-	if req.ServiceType == 1 {
+	connectionDatum.Type = req.Type
+	connectionDatum.Detail = req.Detail
 
-		connectionDatum.Type = req.Type
-		connectionDatum.Detail = req.Detail
-	} else if req.ServiceType == 2 {
-		credentialDatum.Type = req.Type
-		credentialDatum.Detail = req.Detail
-	}
 	dbErr := scs.db.Transaction(func(tx *gorm.DB) error {
 		mainErr := tx.Model(models.ServiceConnection{}).Create(&mainDatum)
 		if mainErr.Error != nil {
 			return mainErr.Error
 		}
-		if req.ServiceType == 1 {
-			connectionDatum.MainID = mainDatum.ID
-			connErr := tx.Model(models.ServiceConnectionDetails{}).Create(&connectionDatum)
-			if connErr.Error != nil {
-				return connErr.Error
-			}
-		}
-		if req.ServiceType == 2 {
-			credentialDatum.MainID = mainDatum.ID
-			credentialErr := tx.Model(models.ServiceConnectionCredentials{}).Create(&credentialDatum)
-			if credentialErr.Error != nil {
-				return credentialErr.Error
-			}
+		connectionDatum.MainID = mainDatum.ID
+		connErr := tx.Model(models.ServiceConnectionDetails{}).Create(&connectionDatum)
+		if connErr.Error != nil {
+			return connErr.Error
 		}
 		return nil
 	})
@@ -81,7 +62,6 @@ func (scs *ServiceConnectionService) UpdateServiceConnection(req *req.ServiceCon
 		return nil, errors.New("非法标识")
 	}
 	var mainDatum = models.ServiceConnection{}
-	var credentialDatum = models.ServiceConnectionCredentials{}
 	var connectionDatum = models.ServiceConnectionDetails{}
 	mainRes := scs.db.Model(models.ServiceConnection{}).Where("id=?", req.ID).First(&mainDatum)
 	if mainRes.Error != nil {
@@ -89,17 +69,11 @@ func (scs *ServiceConnectionService) UpdateServiceConnection(req *req.ServiceCon
 	}
 	mainDatum.ServiceType = req.ServiceType
 	mainDatum.Name = req.Name
-	if req.ServiceType == 1 {
-		connRes := scs.db.Model(models.ServiceConnectionDetails{}).Where("main_id=?", req.ID).First(&connectionDatum)
-		if connRes.Error != nil {
-			return nil, connRes.Error
-		}
-	} else if req.ServiceType == 2 {
-		credentialRes := scs.db.Model(models.ServiceConnectionCredentials{}).Where("main_id=?", req.ID).First(&credentialDatum)
-		if credentialRes.Error != nil {
-			return nil, credentialRes.Error
-		}
+	connRes := scs.db.Model(models.ServiceConnectionDetails{}).Where("main_id=?", req.ID).First(&connectionDatum)
+	if connRes.Error != nil {
+		return nil, connRes.Error
 	}
+
 	dbErr := scs.db.Transaction(func(tx *gorm.DB) error {
 		currtime := time.Now()
 		mainDatum.UpdateTime = &currtime
@@ -107,20 +81,13 @@ func (scs *ServiceConnectionService) UpdateServiceConnection(req *req.ServiceCon
 		if mainErr.Error != nil {
 			return mainErr.Error
 		}
-		if req.ServiceType == 1 {
-			connectionDatum.UpdateTime = &currtime
-			connErr := tx.Model(models.ServiceConnectionDetails{}).Where("id=?", connectionDatum.ID).Updates(connectionDatum)
-			if connErr.Error != nil {
-				return connErr.Error
-			}
+		connectionDatum.Detail = req.Detail
+		connectionDatum.UpdateTime = &currtime
+		connErr := tx.Model(models.ServiceConnectionDetails{}).Where("id=?", connectionDatum.ID).Updates(connectionDatum)
+		if connErr.Error != nil {
+			return connErr.Error
 		}
-		if req.ServiceType == 2 {
-			credentialDatum.UpdateTime = &currtime
-			credentialErr := tx.Model(models.ServiceConnectionCredentials{}).Where("id=?", credentialDatum.ID).Updates(credentialDatum)
-			if credentialErr.Error != nil {
-				return credentialErr.Error
-			}
-		}
+
 		return nil
 	})
 	if dbErr != nil {
