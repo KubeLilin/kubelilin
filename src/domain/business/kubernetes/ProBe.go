6bcbea194fbbaf75449b009209ce7e6fb0975fd6
Cui.Jiaqing <291527070@qq.com>
2022-12-14 09:14:34 +0800

    探针创建

--- src/domain/business/kubernetes/ProBe.go
+++ src/domain/business/kubernetes/ProBe.go
@@ -1,6 +1,7 @@
 package kubernetes
 
 import (
+	"errors"
 	"gorm.io/gorm"
 	"kubelilin/api/dto/requests"
 	"kubelilin/domain/database/models"
@@ -19,27 +20,32 @@ func NewProBeService(_db *gorm.DB) *ProBeService {
 // CreateProBe 创建声明周期探针/**
 func (ds *ProBeService) CreateProBe(proReq *requests.ProbeRequest) {
 	ds.db.Transaction(func(tx *gorm.DB) error {
+		mainContainer := &models.SgrTenantDeploymentsContainers{}
+		tx.Model(models.SgrTenantDeploymentsContainers{}).Where("deploy_id=? and is_main=1", proReq.DpId).First(mainContainer)
+		if mainContainer == nil {
+			return errors.New("can't find the sole container of development ")
+		}
 		if proReq.EnableReadiness {
-			probe := models.SgrDeploymentProbe{}
-			probe.DpID = proReq.DpId
+			probe := models.DeploymentContainerLifecycleCheck{}
+			probe.DeploymentID = proReq.DpId
 			probe.Type = proReq.ReadinessType
 			probe.Port = proReq.ReadinessPort
 			probe.Type = READINESS
 			probe.Path = proReq.ReadinessUrl
-			probe.ReqScheme = proReq.ReadinessReqScheme
+			probe.Scheme = proReq.ReadinessReqScheme
 			probe.PeriodSeconds = proReq.ReadinessPeriodSeconds
 			probe.InitialDelaySeconds = proReq.ReadinessInitialDelaySeconds
 			err := tx.Model(models.SgrDeploymentProbe{}).Save(&probe).Error
 			return err
 		}
 		if proReq.EnableLiveness {
-			probe := models.SgrDeploymentProbe{}
-			probe.DpID = proReq.DpId
+			probe := models.DeploymentContainerLifecycleCheck{}
+			probe.DeploymentID = proReq.DpId
 			probe.Type = proReq.LivenessType
 			probe.Port = proReq.LivenessPort
 			probe.Path = proReq.LivenessUrl
 			probe.Type = LIVENESS
-			probe.ReqScheme = proReq.LivenessReqScheme
+			probe.Scheme = proReq.LivenessReqScheme
 			probe.PeriodSeconds = proReq.LivenessPeriodSeconds
 			probe.InitialDelaySeconds = proReq.LivenessInitialDelaySeconds
 			err := tx.Save(&probe).Error
