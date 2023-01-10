d2c65459311fc46502f40c9e2bc6dbc947450b20
yoyofx <zl.hxd@hotmail.com>
2023-01-10 17:20:12 +0800

    fixed CVE-2022-41717

--- src/domain/business/kubernetes/apply/apply_deploy_volume.go
+++ src/domain/business/kubernetes/apply/apply_deploy_volume.go
@@ -28,21 +28,26 @@ func ApplyVolume(deployConfiguration *appsapplyv1.DeploymentApplyConfiguration, 
 		deployConfiguration.Spec.Template.Spec.WithVolumes(volumeApplyConfigurationList...)
 	}
 
-	//if dpc.VolumeMounts != "" {
-	//	var volumeMouteApplyConfigurationList []*corev1.VolumeMountApplyConfiguration
-	//	var volumeMounts []resultV1.VolumeMount
-	//	utils.StringToJson(dpc.VolumeMounts, &volumeMounts)
-	//	var containerApplyConfig corev1.ContainerApplyConfiguration
-	//	if len(deployConfiguration.Spec.Template.Spec.Containers) > 0 {
-	//		containerApplyConfig = deployConfiguration.Spec.Template.Spec.Containers[0]
-	//		//containerApplyConfig.VolumeMounts
-	//		for _, volumeMount := range volumeMounts {
-	//			volumeMountApplyConfiguration := corev1.VolumeMount().
-	//				WithName(volumeMount.Volume).
-	//				WithMountPath(volumeMount.DesPath)
-	//			if volumeMount
-	//		}
-	//	}
-	//}
-
+	if dpc.VolumeMounts != "" {
+		var volumeMouteApplyConfigurationList []*corev1.VolumeMountApplyConfiguration
+		var volumeMounts []resultV1.VolumeMount
+		utils.StringToJson(dpc.VolumeMounts, &volumeMounts)
+		var containerApplyConfig corev1.ContainerApplyConfiguration
+		if len(deployConfiguration.Spec.Template.Spec.Containers) > 0 {
+			containerApplyConfig = deployConfiguration.Spec.Template.Spec.Containers[0]
+			for _, volumeMount := range volumeMounts {
+				volumeMountApplyConfiguration := corev1.VolumeMount().
+					WithName(volumeMount.Volume).
+					WithMountPath(volumeMount.DesPath)
+				if volumeMount.SubPathType == "subPath" {
+					volumeMountApplyConfiguration.WithSubPath(volumeMount.SubPath)
+				}
+				if volumeMount.MountType == "readonly" {
+					volumeMountApplyConfiguration.WithReadOnly(true)
+				}
+				volumeMouteApplyConfigurationList = append(volumeMouteApplyConfigurationList, volumeMountApplyConfiguration)
+			}
+			containerApplyConfig.WithVolumeMounts(volumeMouteApplyConfigurationList...)
+		}
+	}
 }
