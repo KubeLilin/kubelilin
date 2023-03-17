2a0de2c22f24d0c1be331c4665747a1ca7143b0b
yoyofx <zl.hxd@hotmail.com>
2023-03-17 16:17:18 +0800

    生命周期命令 可用性配置

--- src/domain/business/kubernetes/apply/apply_deploy_lifecycle.go
+++ src/domain/business/kubernetes/apply/apply_deploy_lifecycle.go
@@ -36,12 +36,14 @@ func ApplyLifecycle(deployConfiguration *appsapplyv1.DeploymentApplyConfiguratio
 	if len(deployConfiguration.Spec.Template.Spec.Containers) > 0 {
 		containerApplyConfig = deployConfiguration.Spec.Template.Spec.Containers[0]
 	}
-	containerApplyConfig.Lifecycle = corev1.Lifecycle()
-	if dpc.Poststart != "" {
-		containerApplyConfig.Lifecycle.WithPostStart(corev1.Handler().WithExec(corev1.ExecAction().WithCommand(dpc.Poststart)))
-	}
-	if dpc.Podstop != "" {
-		containerApplyConfig.Lifecycle.WithPreStop(corev1.Handler().WithExec(corev1.ExecAction().WithCommand(dpc.Podstop)))
+	if dpc.EnableLife != nil && *dpc.EnableLife > 0 {
+		containerApplyConfig.Lifecycle = corev1.Lifecycle()
+		if dpc.Poststart != "" {
+			containerApplyConfig.Lifecycle.WithPostStart(corev1.Handler().WithExec(corev1.ExecAction().WithCommand(dpc.Poststart)))
+		}
+		if dpc.Podstop != "" {
+			containerApplyConfig.Lifecycle.WithPreStop(corev1.Handler().WithExec(corev1.ExecAction().WithCommand(dpc.Podstop)))
+		}
 	}
 	deployConfiguration.Spec.Template.Spec.Containers[0] = containerApplyConfig
 
