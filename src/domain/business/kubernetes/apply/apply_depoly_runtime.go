2986d91a03a697865dc812b2cbeb7d3322ea1a65
yoyofx <zl.hxd@hotmail.com>
2023-03-31 10:47:46 +0800

    sidecar 配置区分环境配置

--- src/domain/business/kubernetes/apply/apply_depoly_runtime.go
+++ src/domain/business/kubernetes/apply/apply_depoly_runtime.go
@@ -18,10 +18,20 @@ func ApplyRuntime(deployConfiguration *appsapplyv1.DeploymentApplyConfiguration,
 			//	dapr.io/app-id: "myapp"
 			//	dapr.io/app-port: "8223"
 			//	dapr.io/enable-api-logging: "true"
+			//	dapr.io/enable-metrics: "true"
+			//	dapr.io/metrics-port: "9090"
 			deployConfiguration.Spec.Template.Annotations["dapr.io/enabled"] = "true"
 			deployConfiguration.Spec.Template.Annotations["dapr.io/app-id"] = dp.Name
 			deployConfiguration.Spec.Template.Annotations["dapr.io/app-port"] = utils.ToString(dp.ServicePort)
 			deployConfiguration.Spec.Template.Annotations["dapr.io/enable-api-logging"] = "true"
+			// Enable metrics : https://docs.dapr.io/operations/monitoring/metrics/metrics-overview/
+			deployConfiguration.Spec.Template.Annotations["dapr.io/enable-metrics"] = "true"
+			deployConfiguration.Spec.Template.Annotations["dapr.io/metrics-port"] = "9090"
+			//# This enables JSON-formatted logging
+			//	dapr.io/log-as-json: "true"
+			deployConfiguration.Spec.Template.Annotations["dapr.io/log-as-json"] = "true"
+			// Enables tracing : https://docs.dapr.io/operations/monitoring/tracing/zipkin/
+			//deployConfiguration.Spec.Template.Annotations["dapr.io/config"] = "tracing"
 			//  Sidecar Resources:
 			//  Cpu Limit: 300m, Request: 100m
 			//  Memory Limit: 1000Mi, Request: 250Mi
@@ -29,10 +39,17 @@ func ApplyRuntime(deployConfiguration *appsapplyv1.DeploymentApplyConfiguration,
 			//	dapr.io/sidecar-memory-limit
 			//	dapr.io/sidecar-cpu-request
 			//	dapr.io/sidecar-memory-request
-			deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-cpu-limit"] = "300m"
-			deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-memory-limit"] = "1000Mi"
-			deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-cpu-request"] = "100m"
-			deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-memory-request"] = "250Mi"
+			if dp.Level == "dev" || dp.Level == "test" { // dev/test
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-cpu-limit"] = "100m"
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-memory-limit"] = "100Mi"
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-cpu-request"] = "50m"
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-memory-request"] = "50Mi"
+			} else { // prod
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-cpu-limit"] = "300m"
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-memory-limit"] = "1000Mi"
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-cpu-request"] = "100m"
+				deployConfiguration.Spec.Template.Annotations["dapr.io/sidecar-memory-request"] = "250Mi"
+			}
 		}
 	}
 }
